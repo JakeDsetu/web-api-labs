bc86c5553247c66532b80c005640c99ec07f2a63
JakeDsetu <20101923@mail.wit.ie>
2025-04-27 14:27:11 +0100

    Added Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+import cors from 'cors';
+import usersRouter from './api/users';
+
 
 dotenv.config();
 
@@ -16,14 +19,18 @@ const errHandler = (err, req, res, next) => {
 
 const app = express();
 
+// Enable CORS for all requests
+app.use(cors());
+
 const port = process.env.PORT;
 
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
 
-app.use(errHandler);
+app.use('/api/users', usersRouter);
 
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
