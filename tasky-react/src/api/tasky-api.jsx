499e84d2987b5710651657145aaf895e49753272
JakeDsetu <20101923@mail.wit.ie>
2025-04-29 16:48:53 +0100

    Added auth functions to React app

--- tasky-react/src/api/tasky-api.jsx
+++ tasky-react/src/api/tasky-api.jsx
@@ -5,6 +5,29 @@ export const getTasks = async () => {
         return res.json();
 };
 
+export const login = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
+
+export const signup = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
+
+
 export const addTask = async(data) => {
     const res = await fetch(
         `http://localhost:8080/api/tasks`,
